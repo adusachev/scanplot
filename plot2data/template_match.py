00ccde4cd6e79b20e29c36e2630a04525a1118a4
adusachev <usachev.alexe@gmail.com>
2024-03-13 23:32:05 +0300

    experimental feature with CCOEFF

--- plot2data/template_match.py
+++ plot2data/template_match.py
@@ -5,6 +5,7 @@ import numpy as np
 import cv2 as cv
 from sklearn.cluster import AgglomerativeClustering, MeanShift, DBSCAN
 from dotenv import load_dotenv
+import numpy_indexed as npi
 
 from setup_logger import logger
 
@@ -77,6 +78,50 @@ def detect_points(
     
     return points
 
+
+def detect_points_v1(
+    convolution_map: np.ndarray,
+    max_value: float,
+    tolerance: float,
+    convolution_map_ccoef: np.ndarray
+) -> np.ndarray:
+    """
+    Берет convolution_map_ccoef полученную из cv.TM_CCOEFF_NORMED, 
+     и делает по ней дополнительную фильтрацию - смотрит, где convolution_map_ccoef не NaN
+    """
+    max_positions = np.where( np.isclose(convolution_map, max_value, atol=tolerance) )
+    y, x = max_positions
+    max_points = np.array([x, y]).T
+    
+    area_of_interest = np.where( np.isnan(convolution_map_ccoef) == False )
+    y, x = area_of_interest
+    area_of_interest_points = np.array([x, y]).T
+
+    points = npi.intersection(max_points, area_of_interest_points)
+
+    return points
+
+
+
+def find_tolerance_limit_v1(
+    convolution_map: np.ndarray, 
+    convolution_map_ccoef: np.ndarray
+) -> float:
+    """
+    Использует detect_points_v1
+    """
+    tolerance_range = np.arange(0, 2, 0.001)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(convolution_map)
+
+    for i, tolerance in enumerate(tolerance_range):
+        # points = detect_points(convolution_map, max_val, tolerance)
+        points = detect_points_v1(convolution_map, max_val, tolerance, convolution_map_ccoef)
+        points_number = len(points)
+        if points_number > 1000:
+            tolerance_limit = tolerance_range[i-1]
+            return tolerance_limit
+        
+    return tolerance_range[-1]
 
 
 def find_tolerance_limit(convolution_map: np.ndarray) -> float:
