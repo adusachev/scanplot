15344f898352679533f5a3c8b8245dd683839b20
adusachev <usachev.alexe@gmail.com>
2024-05-25 14:24:23 +0300

    little fix in git hook script

--- validate_files_size.sh
+++ validate_files_size.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
+# limit file fize (in megabytes)
 FILE_SIZE_LIMIT=50
 
 
 function check_file_size {
     size=$(du -m $1 | awk '{print $1}')
     if (($size > $FILE_SIZE_LIMIT)); then
-       echo "Size of file $1 is greater than ${FILE_SIZE_LIMIT}M"
+       echo "Commit rejected, file $1 has size $size that is greater than ${FILE_SIZE_LIMIT}M"
        exit 1
     fi
 }
