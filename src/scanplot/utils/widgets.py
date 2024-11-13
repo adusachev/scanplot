7e4ba922597d8e960c8e382aa15ee03637179c1a
adusachev <adusachev@yandex.ru>
2024-11-13 11:24:05 +0300

    fix ipywidgets slider: set continuous_update=True, create init_slider() func

--- src/scanplot/utils/widgets.py
+++ src/scanplot/utils/widgets.py
@@ -1,32 +1,35 @@
 import ipywidgets
 
 
-points_num_slider = ipywidgets.FloatSlider(
-    value=20,
-    min=0,
-    max=100,
-    step=0.1,
-    description='Points Number:',
-    disabled=False,
-    continuous_update=False,
-    orientation='horizontal',
-    readout=True,
-    readout_format='d',
-    layout=ipywidgets.Layout(width='500px'),
-    style={'description_width': 'initial'}
-)
+def init_points_num_slider():
+    return ipywidgets.FloatSlider(
+        value=20,
+        min=0,
+        max=100,
+        step=0.1,
+        description='Points Number:',
+        disabled=False,
+        continuous_update=True,
+        orientation='horizontal',
+        readout=True,
+        readout_format='d',
+        layout=ipywidgets.Layout(width='500px'),
+        style={'description_width': 'initial'}
+    )
 
-points_density_slider = ipywidgets.FloatSlider(
-    value=20,
-    min=0,
-    max=100,
-    step=1,
-    description='Points Density:',
-    disabled=False,
-    continuous_update=False,
-    orientation='horizontal',
-    readout=True,
-    readout_format='d',
-    layout=ipywidgets.Layout(width='500px'),
-    style={'description_width': 'initial'}
-)
+
+def init_points_density_slider():
+    return ipywidgets.FloatSlider(
+        value=20,
+        min=0,
+        max=100,
+        step=1,
+        description='Points Density:',
+        disabled=False,
+        continuous_update=True,
+        orientation='horizontal',
+        readout=True,
+        readout_format='d',
+        layout=ipywidgets.Layout(width='500px'),
+        style={'description_width': 'initial'}
+    )
