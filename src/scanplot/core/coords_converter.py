593ac03bc586c7e53da62d91694c7f96fbfe7424
adusachev <adusachev@yandex.ru>
2024-11-27 23:58:09 +0300

    refactor CoordinatesConverter methods

--- src/scanplot/core/coords_converter.py
+++ src/scanplot/core/coords_converter.py
@@ -26,36 +26,42 @@ class CoordinatesConverter:
         self.y_logscale = y_logscale
 
     def pixel_to_real(self, x_pixel: int, y_pixel: int) -> tuple[float, float]:
-        pass
+        x_real = self._convert_x_axis_linear(x_pixel)
+        y_real = self._convert_y_axis_linear(y_pixel)
 
-    def real_to_pixel(self, x_real: int, y_real: int) -> tuple[int, int]:
-        raise NotImplementedError
+        return x_real, y_real
 
-    def _pixel_to_real_linear(self, x_px: int, y_px: int) -> tuple[float, float]:
-        # X axis
+    def _convert_x_axis_linear(self, x_pixel: int) -> float:
+        """
+        Converts x-axis pixel value into real value
+        """
         alpha_x = (self.x_max_real - self.x_min_real) / (self.x_max_px - self.x_min_px)
-        if x_px >= self.x_min_px:
-            x_real = alpha_x * (x_px - self.x_min_px) + self.x_min_real
-        else:
-            x_real = self.x_min_real - alpha_x * (self.x_min_px - x_px)
 
-        # Y axis
+        if x_pixel >= self.x_min_px:
+            x_real = alpha_x * (x_pixel - self.x_min_px) + self.x_min_real
+        else:
+            x_real = self.x_min_real - alpha_x * (self.x_min_px - x_pixel)
+
+        return x_real
+
+    def _convert_y_axis_linear(self, y_pixel: int) -> float:
+        """
+        Converts y-axis pixel value into real value
+         (assumed that Y pixels coords grow from top to bottom)
+        """
         assert self.y_min_px > self.y_max_px, "Y pixels coords grow from top to bottom"
 
         alpha_y = (self.y_max_real - self.y_min_real) / (self.y_min_px - self.y_max_px)
 
-        if y_px >= self.y_min_px:
-            y_real = self.y_min_real + alpha_y * (self.y_min_px - y_px)
+        if y_pixel >= self.y_min_px:
+            y_real = self.y_min_real + alpha_y * (self.y_min_px - y_pixel)
         else:
-            y_real = self.y_min_real - alpha_y * (y_px - self.y_min_px)
+            y_real = self.y_min_real - alpha_y * (y_pixel - self.y_min_px)
 
-        return x_real, y_real
+        return y_real
 
-    def _convert_x_logscale(self):
-        pass
+    def _convert_x_axis_logscale(self):
+        raise NotImplementedError
 
-    def _convert_y_logscale(self):
-        pass
-
-    def _convert_xy_logscale(self):
-        pass
+    def _convert_y_axis_logscale(self):
+        raise NotImplementedError
