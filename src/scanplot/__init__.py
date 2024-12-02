f3b22338da438a52fd79483cbcc2aee0cb0ce80c
adusachev <adusachev@yandex.ru>
2024-12-02 17:00:05 +0300

    fix import logger config

--- src/scanplot/__init__.py
+++ src/scanplot/__init__.py
@@ -1,6 +1,7 @@
 import logging
+from logging.config import dictConfig
 
 from .logger_settings import logger_config
 
-logging.config.dictConfig(logger_config)
+dictConfig(logger_config)
 logger = logging.getLogger(__name__)
